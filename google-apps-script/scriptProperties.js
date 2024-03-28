80fe1c88e298263356580993c64c46850333b75d
Ian NP. Campbell <inpcampbell@woodburnsd.org>
2024-03-28 11:44:32 -0700

    column idx

--- google-apps-script/scriptProperties.js
+++ google-apps-script/scriptProperties.js
@@ -1,19 +1,37 @@
 
-function readProperties(key){
+function readProperty(key){
      try{
           const scriptProperties = PropertiesService.getScriptProperties();
-          scriptProperties.getProperty(key)
+          const property = JSON.parse(scriptProperties.getProperty(key));
+          return property;
      }catch(e){
+         console.log(e)
           console.log(`Unable to retrieve value for ${key}`)
      }
 }
 
-function writeProperties(key,value){
+function tryIt(){
+  readProperty("SHEET_NAMES")
+}
+
+function writeProperty(key,value){
      try{
           const scriptProperties = PropertiesService.getScriptProperties();
-          scriptProperties.setProperty(key,value)
+          scriptProperties.setProperty(key,JSON.stringify(value));
      }catch(e){
-          console.log(`Script property for ${key} not set`)
+          console.log(`Script property for ${key} not set`);   
      }
 
+}
+
+function deleteProperties(){
+     try {
+     // Get user properties in the current script.
+     const userProperties = PropertiesService.getScriptProperties();
+     // Delete all user properties in the current script.
+     userProperties.deleteAllProperties();
+     } catch (err) {
+     // TODO (developer) - Handle exception
+     console.log('Failed with error %s', err.message);
+     }
 }
--- google-apps-script/sideBar.js
+++ google-apps-script/sideBar.js
@@ -13,18 +13,34 @@ function createSpreadsheetOpenTrigger() {
 }
 
 function serverSideGetSheetNames(){
-    const ss = getSs();
-    const sheets = ss.getSheets();
-    const sheetNames = sheets.map(sheet => sheet.getSheetName())
-    Logger.log(sheetNames);
-    return sheetNames;
+    const start = new Date()
+    const savedNames = readProperty("SHEET_NAMES");
+    // console.log("Save data is:"+savedNames)
+    
+    if(savedNames){
+      // console.log("Property has data; go ahead and read it")
+      const end = new Date();
+      console.log(`Reading from script properties took ${(end - start) / 1000}`)
+      return savedNames;
+    } else{
+      console.log("Property was empty; saved the data")
+        const ss = getSs();
+        const sheets = ss.getSheets();
+        const sheetNames = sheets.map(sheet => sheet.getSheetName())
+        writeProperty("SHEET_NAMES",sheetNames);
+        const end = new Date();
+        console.log(`Reading from the spreadsheet took ${(end - start) / 1000}`)
+        return sheetNames;
+    }
+
 }
 
 function serverSideGetHeaders(sheetName,id){
+    // Logger.log(`The sheetname received is ${sheetName}`)
     const sheet = getSheet(sheetName);
     const headerRange = sheet.getRange(1,1,1,sheet.getLastColumn());
     const headerVals = headerRange.getValues();
-    Logger.log(headerVals);
+    // Logger.log(headerVals);
     const returnObj = {
         headers: headerVals,
         id: id
